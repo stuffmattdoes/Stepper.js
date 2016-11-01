3c8536427b207ad15dab6bbad1e1c8ca96bc8a40
Matthew J. Morrison <m.james.morrison00@gmail.com>
2016-10-31 21:40:36 -0400

    Initial commit

---
+++ js/Stepper.js
@@ -0,0 +1,83 @@
+
+/*
+
+    Stepper.js
+
+    Description: Step through multiple elements and activate them one at a time
+    Author: Matthew Morrison @stuffmattdoesnt
+    License: MIT
+    URL: stuffmattdoes.com
+    Version: 1.0
+
+    TODO
+
+*/
+
+(function() {
+     this.Stepper = function(container, params) {
+
+         console.log("Stepper!");
+
+        // ----------
+        // Properties
+        // ----------
+
+        var defaults = {
+            containerActiveClass: '.stepper-container-active',
+            containerCompleteClass: '.stepper-container-complete',
+            direction: "forward",
+            item: '.stepper-item',
+            itemActiveClass: 'stepper-item-active',
+            speed: 500
+        }
+
+        var stepperInstance = this;
+
+        // Create options by extending defaults with the passed in arugments
+        if (arguments[1] && typeof arguments[1] === "object") {
+            this.options = extendDefaults(defaults, arguments[1]);
+        }
+
+        // Utility method to extend defaults with user options
+        function extendDefaults(source, properties) {
+
+            for (var property in properties) {
+                if (properties.hasOwnProperty(property)) {
+                    source[property] = properties[property];
+                }
+            }
+
+            // console.log(source);
+            return source;
+        }
+
+        console.log(this);
+
+        // --------------
+        // Public Methods
+        // --------------
+
+        this.stepIt = function() {
+
+            console.log(this);
+
+            // Variables
+            var containerCompleteClass = stepperInstance.options.containerCompleteClass,
+                item = stepperInstance.options.item,
+                itemActiveClass = stepperInstance.options.itemActiveClass,
+                speed = stepperInstance.options.speed,
+                nextItem = 0;
+
+            $(container).removeClass(containerCompleteClass);
+
+            $(container).find($(item)).each(function(index, value) {
+                setTimeout(function() {
+                    $(value).addClass(itemActiveClass);
+                }, speed * (index + 1));
+            });
+
+        }();
+
+    }
+
+})();
---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Main javascript
+
+$myStepper = new Stepper($('.stepper-container'), {
+    speed: 1000
+});
+
+// $('.stepper-container .stepper-item').each(function(index, value) {
+//
+//     setTimeout(function() {
+//         console.log(index);
+//         $(value).addClass('active');
+//     }, 250 * (index + 1));
+//
+// });
